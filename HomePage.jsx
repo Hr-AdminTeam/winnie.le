6c74f09c21dd7a607924cb0d0a89a5804f8dfc26
Hr-AdminTeam <kimthuy2493@gmail.com>
2025-09-10 17:21:34 +0700

    HomePage.jsx

---
+++ HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContentForm from '../components/ContentForm';
+
+const HomePage = () => (
+  <div>
+    <h1>Trang chủ</h1>
+    <ContentForm />
+  </div>
+);
+
+export default HomePage;
